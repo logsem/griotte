4b64d9aed67d5ae7b96c33676306c5778a7cf95a
June Rousseau <june.rousseau@cs.au.dk>
2025-03-27 14:40:04 +0100

    WIP switcher spec: layout tstack resource

--- theories/examples/switcher/switcher_spec.v
+++ theories/examples/switcher/switcher_spec.v
@@ -4,6 +4,46 @@ From cap_machine Require Import logrel interp_weakening addr_reg_sample rules pr
 From cap_machine Require Import multiple_updates.
 From cap_machine Require Import switcher.
 
+Section TStack.
+  Context {Σ:gFunctors} {A : Type}.
+
+  Definition tstack_full (l : list A) : iProp Σ. Admitted.
+  Definition tstack_frag (l : list A) (n : nat) : iProp Σ. Admitted.
+
+  Lemma tstack_agree (l l' : list A) (n : nat) :
+    tstack_full l
+    -∗ tstack_frag l' n
+    -∗ ⌜ ∃ l0 l1, length l0 = n ∧ l = l0++l'++l1 ⌝
+  .
+  Admitted.
+
+  Lemma tstack_full_unique (l l' : list A) :
+    tstack_full l -∗ tstack_full l' -∗ False.
+  Admitted.
+
+  Lemma tstack_frag_unique (l l' : list A) (n n' : nat) :
+    n <= n' < (n + length l')
+    -> tstack_frag l n
+    -∗ tstack_frag l' n'
+    -∗ False.
+  Admitted.
+
+  Lemma tstack_frag_combine (l l' : list A) (n n' : nat) :
+    n' = (n + length l)
+    -> tstack_frag l n ∗ tstack_frag l' n' ⊣⊢ tstack_frag (l++l') n.
+  Admitted.
+
+  Lemma tstack_frag_combine_cons (a : A) (l : list A) (n : nat) :
+    tstack_frag (a::l) n ⊣⊢ tstack_frag [a] n ∗ tstack_frag l (n+1).
+  Proof.
+    iStartProof.
+    change (a::l) with ([a]++l).
+    rewrite tstack_frag_combine; last done.
+    by iSplit; iIntros "H".
+  Qed.
+
+End TStack.
+
 Section Switcher.
   Context
     {Σ:gFunctors}
@@ -49,20 +89,23 @@ Section Switcher.
   Definition switcher_inv
     (b_switcher e_switcher a_switcher_cc : Addr)
     (ot_switcher : OType) : iProp Σ :=
-    ∃ (b_tstk e_tstk a_tstk : Addr) (tstk : list Word),
+    ∃ (b_tstk e_tstk a_tstk : Addr) (tstk_prev tstk_next : list Word),
      mtdc ↦ₛᵣ WCap RWL Local b_tstk e_tstk a_tstk
      ∗ ⌜ SubBounds
          b_switcher e_switcher
          a_switcher_cc (a_switcher_cc ^+ length switcher_instrs)%a ⌝
      ∗ codefrag a_switcher_cc switcher_instrs
      ∗ b_switcher ↦ₐ WSealRange (true,true) Global ot_switcher (ot_switcher^+1)%ot ot_switcher
-     ∗ [[ (a_tstk ^+1)%a, e_tstk ]] ↦ₐ [[ tstk ]]
+     ∗ [[ b_tstk, (a_tstk ^+1)%a ]] ↦ₐ [[ tstk_prev ]]
+     ∗ [[ (a_tstk ^+1)%a, e_tstk ]] ↦ₐ [[ tstk_next ]]
+     ∗ tstack_full (tstk_prev ++ tstk_next)
+     ∗ tstack_frag tstk_next (finz.dist (a_tstk ^+1)%a e_tstk)
      ∗ seal_pred ot_switcher ot_switcher_prop
   .
 
   (* TODO move *)
   Definition list_max_positive (l : list positive) := fold_right Pos.max xH l.
-  Lemma fresh_max (l : list positive) (i : positive) :
+  Lemma fresh_list_max (l : list positive) (i : positive) :
     ( (list_max_positive l) < i )%positive -> i ∉ l.
   Proof.
     induction l; intros Hle.
@@ -77,89 +120,63 @@ Section Switcher.
       split; [lia | by apply IHl].
   Qed.
 
-  (* Definition max_sts_name1 (W : WORLD) := *)
-  (*   list_max_positive *)
-  (*   (fst <$> (map_to_list (gset_to_gmap 0 (dom (loc1 W))))). *)
-  (* Definition max_sts_name2 (W : WORLD) := *)
-  (*   list_max_positive *)
-  (*   (fst <$> (map_to_list (gset_to_gmap 0 (dom (loc2 W))))). *)
-
-  Definition gset_to_list {A : Type} `{Countable A} (g : gset A) :=
-   (fst <$> (map_to_list (gset_to_gmap 0 g))).
+  Definition gset_max_positive (g : gset positive) := list_max_positive (elements g).
+  Lemma fresh_gset_max (g : gset positive) (i : positive) :
+    ( (gset_max_positive g) < i )%positive -> i ∉ g.
+  Proof.
+    intros Hmax.
+    rewrite /gset_max_positive in Hmax.
+    apply fresh_list_max in Hmax.
+    set_solver.
+  Qed.
 
   Definition fresh_cus_name (W : WORLD) : positive :=
-    let WL1 := list_max_positive (gset_to_list (dom (loc1 W))) in
-    let WL2 := list_max_positive (gset_to_list (dom (loc2 W))) in
+    let WL1 := gset_max_positive (dom (loc1 W)) in
+    let WL2 := gset_max_positive (dom (loc2 W)) in
     (Pos.succ ( WL1 `max` WL2 )%positive).
-
-  Lemma elem_of_gset_to_list_iff {A : Type} `{Countable A} (x : A) (g : gset A) :
-    x ∈ gset_to_list g <-> x ∈ g.
-  Proof.
-    rewrite /gset_to_list.
-    induction g using set_ind_L.
-    - rewrite gset_to_gmap_empty map_to_list_empty //=.
-      set_solver.
-    - rewrite gset_to_gmap_union_singleton map_to_list_insert /=.
-      2: { by apply lookup_gset_to_gmap_None. }
-      rewrite elem_of_cons elem_of_union elem_of_singleton.
-      naive_solver.
-  Qed.
-
-  Lemma not_elem_of_gset_to_list_iff {A : Type} `{Countable A} (x : A) (g : gset A) :
-    x ∉ gset_to_list g <-> x ∉ g.
-  Proof.
-    apply not_iff_compat.
-    apply elem_of_gset_to_list_iff.
-  Qed.
-
 
   Lemma fresh_cus_name_fresh1 (W : WORLD) :
     fresh_cus_name W ∉ (dom (loc1 W)).
   Proof.
     rewrite /fresh_cus_name.
-    set ( n1 :=  list_max_positive (gset_to_list (dom (loc1 W)))).
-    set ( n2 :=  list_max_positive (gset_to_list (dom (loc2 W)))).
+    set ( n1 := gset_max_positive (dom (loc1 W))).
+    set ( n2 := gset_max_positive (dom (loc2 W))).
     destruct (Pos.max_dec n1 n2) as [Hmax | Hmax].
     - rewrite Hmax.
-      apply not_elem_of_gset_to_list_iff.
-      apply fresh_max.
-      subst n1; lia.
+      assert (n1 < Pos.succ n1)%positive as Hn1 by lia.
+      by apply fresh_gset_max in Hn1.
     - assert (n1 <= n2)%positive as H2_le_1 by (by apply Pos.max_r_iff).
       rewrite Hmax.
-      apply not_elem_of_gset_to_list_iff.
-      apply fresh_max.
-      subst n1; lia.
+      assert (n1 < Pos.succ n2)%positive as Hn1 by lia.
+      by apply fresh_gset_max in Hn1.
   Qed.
 
   Lemma fresh_cus_name_fresh2 (W : WORLD) :
     fresh_cus_name W ∉ (dom (loc2 W)).
   Proof.
     rewrite /fresh_cus_name.
-    set ( n1 :=  list_max_positive (gset_to_list (dom (loc1 W)))).
-    set ( n2 :=  list_max_positive (gset_to_list (dom (loc2 W)))).
+    set ( n1 := gset_max_positive (dom (loc1 W))).
+    set ( n2 := gset_max_positive (dom (loc2 W))).
     destruct (Pos.max_dec n1 n2) as [Hmax | Hmax].
-    - assert (n2 <= n1)%positive as H1_le_2 by (by apply Pos.max_l_iff).
+    - assert (n2 <= n1)%positive as H2_le_1 by (by apply Pos.max_l_iff).
       rewrite Hmax.
-      apply not_elem_of_gset_to_list_iff.
-      apply fresh_max.
-      subst n2; lia.
+      assert (n2 < Pos.succ n1)%positive as Hn1 by lia.
+      by apply fresh_gset_max in Hn1.
     - rewrite Hmax.
-      apply not_elem_of_gset_to_list_iff.
-      apply fresh_max.
-      subst n2; lia.
+      assert (n2 < Pos.succ n2)%positive as Hn1 by lia.
+      by apply fresh_gset_max in Hn1.
   Qed.
 
-   (* Definition frame_inv (C : CmptName) (i : positive) (P : iProp Σ) := *)
-   (*   (∃ x:bool, sts_state_loc C i x *)
-   (*         ∗ if x then P else emp)%I. *)
+   Definition frame_inv (C : CmptName) (i : positive) (P : iProp Σ) :=
+     (∃ x:bool, sts_state_loc C i x ∗ if x then P else emp)%I.
 
    Definition frame_rel_pub := λ (a b : bool), False.
    Definition frame_rel_priv := λ (a b : bool), True.
-   (* Definition awk_W W i : WORLD := *)
-   (*   (W.1,(<[i:=encode false]>W.2.1, <[i:=(convert_rel frame_rel_pub,convert_rel frame_rel_pub,convert_rel frame_rel_priv)]>W.2.2)). *)
+   Definition frame_W (W : WORLD) : WORLD :=
+     let ι := fresh_cus_name W in
+      <l[ ι := false , ( frame_rel_pub, (frame_rel_pub, frame_rel_priv)) ]l> W.
 
 
-  Definition aaa (W : WORLD) := fst (loc W).
   (* TODO:
      - How to encode the number of registers to pass as arguments?
        A possibility is to use a resource that encodes it.
@@ -192,7 +209,7 @@ Section Switcher.
     in
 
     let POST_COND_SWITCHER :=
-      (▷ ( (∃ (W2 : WORLD) (rmap' : Reg),
+      ( ( (∃ (W2 : WORLD) (rmap' : Reg),
               (* We receive a public future world of the world pre switcher call *)
               ⌜ related_sts_pub_world W0 W2 ⌝
               ∗ ⌜ dom rmap' = all_registers_s ∖ {[ PC ; cgp ; cra ; csp ; ca0 ; ca1 ]} ⌝
@@ -211,20 +228,11 @@ Section Switcher.
         ))%I
     in
 
-    let W0' :=
-      std_update_multiple W0 (finz.seq_between (a_stk ^+ 4)%a e_stk) Temporary
-    in
-    let fresh_sts :=
-      (dom (loc1 W0'))
-    in
-    let W1 :=
-      <l[ fresh_cus_name W0' := false , ( frame_rel_pub, (frame_rel_pub, frame_rel_priv)) ]l> W0'
-    in
-
+    let W1 := frame_W (std_update_multiple W0 (finz.seq_between (a_stk ^+ 4)%a e_stk) Temporary) in
 
     let PRE_COND_SWITCHER :=
-      (na_inv logrel_nais N (switcher_inv b_switcher e_switcher a_switcher_cc ot_switcher)
-      ∗ na_own logrel_nais E
+      (
+      na_own logrel_nais E
       (* Registers *)
       ∗ PC ↦ᵣ WCap XSRW_ g_switcher b_switcher e_switcher a_switcher_cc
       ∗ cgp ↦ᵣ wcgp_caller
@@ -246,19 +254,19 @@ Section Switcher.
       (* Interpretation of the world, at the moment of the switcher_call *)
       ∗ region W0 C ∗ sts_full_world W0 C
 
-      (* Transformation of the world, before the jump to the adversary *)
-      ∗ ( [[ a_stk , e_stk ]] ↦ₐ [[ stk_pre_jmp ]]
-          ∗ region W0 C ∗ sts_full_world W0 C
-          ==∗
-          (*
-            if the user wants to share capabilities pointing in the callee's frame,
-            we need to relinquish the points-to predicates.
-            Which means that the user needs to show safe-to-share.
-           *)
-          interp W1 C (WCap RWL Local (a_stk ^+ 4)%a e_stk (a_stk ^+ 4)%a)
-          (* we forbid the user to share access to caller_save_area *)
-          ∗ [[ a_stk , (a_stk ^+4)%a ]] ↦ₐ [[ stk_caller_save_area ]]
-          ∗ region W1 C ∗ sts_full_world W1 C)
+      (* (* Transformation of the world, before the jump to the adversary *) *)
+      (* ∗ ( [[ a_stk , e_stk ]] ↦ₐ [[ stk_pre_jmp ]] *)
+      (*     ∗ region W0 C ∗ sts_full_world W0 C *)
+      (*     ==∗ *)
+      (*     (* *)
+      (*       if the user wants to share capabilities pointing in the callee's frame, *)
+      (*       we need to relinquish the points-to predicates. *)
+      (*       Which means that the user needs to show safe-to-share. *)
+      (*      *) *)
+      (*     interp W1 C (WCap RWL Local (a_stk ^+ 4)%a e_stk (a_stk ^+ 4)%a) *)
+      (*     (* we forbid the user to share access to caller_save_area *) *)
+      (*     ∗ [[ a_stk , (a_stk ^+4)%a ]] ↦ₐ [[ stk_caller_save_area ]] *)
+      (*     ∗ region W1 C ∗ sts_full_world W1 C) *)
           )%I
     in
 
@@ -267,16 +275,18 @@ Section Switcher.
     dom rmap = all_registers_s ∖ ((dom arg_rmap) ∪ {[ PC ; cgp ; cra ; csp ; cs0 ; cs1 ; ct1 ]} ) ->
     ↑N ⊆ E →
 
-    ( PRE_COND_SWITCHER
-      (* POST-CONDITION *)
-      ∗ POST_COND_SWITCHER
-    )
+    ( na_inv logrel_nais N (switcher_inv b_switcher e_switcher a_switcher_cc ot_switcher)
+       ∗ PRE_COND_SWITCHER
+       ∗ ▷ POST_COND_SWITCHER )
     ⊢ (WP Seq (Instr Executable) {{ λ v, φ v ∨ ⌜v = FailedV⌝ }})%I.
   Proof.
-    intros Hwct1_caller Hstk_caller_save_area Hstk_caller_frm_pre Hstk_pre_jmp Harg_map Hrmap HNE.
-    iIntros "(#Hinv_switcher & Hna
-              & HPC & Hcgp & Hcra & Hcsp & Hct1 & #Hinterp_ct1 & Hcs0 & Hcs1 & Hargmap & Hrmap
-              & Hstk & Hregion & Hworld & Hnext_world & Hφ)".
+    intros Hwct1_caller Hstk_caller_save_area Hstk_caller_frm_pre Hstk_pre_jmp
+      HPRE W1 HPOST Harg_map Hrmap HNE; subst HPRE HPOST.
+    iIntros "[ #Hinv_switcher
+              [ (Hna & HPC & Hcgp & Hcra & Hcsp & Hct1 & #Hinterp_ct1 & Hcs0 & Hcs1
+                & Hargmap & Hrmap & Hstk & Hregion & Hworld)
+              Hφ
+              ] ]".
 
     (* ------------------------------------------------ *)
     (* -------- Prepare resources for the proof ------- *)
@@ -287,7 +297,8 @@ Section Switcher.
       as "(Hswitcher_inv & Hna & Hclose_switcher_inv)" ; auto.
     rewrite /switcher_inv.
     iDestruct "Hswitcher_inv"
-      as (b_tstk e_tstk a_tstk tstk) "(>Hmtdc & >%Hbounds & >Hcode & >Hb_switcher & >Htstk & #Hp_ot_switcher)".
+      as (b_tstk e_tstk a_tstk tstk_prev tstk_next)
+           "(>Hmtdc & >%Hbounds & >Hcode & >Hb_switcher & >Htstk_prev & >Htstk & Hstack_full & Hstack_frag_next & #Hp_ot_switcher)".
     codefrag_facts "Hcode".
     rename H into Hcont_switcher_region; clear H0.
     iHide "Hclose_switcher_inv" as hclose_switcher_inv.
@@ -401,8 +412,8 @@ Section Switcher.
     { admit. (* will fail in the next upcoming instructions *) }
     destruct (decide (b_tstk <= a_tstk))%a as [Htstk_ba|Htstk_ba]; cycle 1.
     { admit. (* will fail in the next upcoming instructions *) }
-    iAssert (⌜ exists tstk_a1 tstk', (tstk = tstk_a1 :: tstk')⌝)%I
-      as %(tstk_a1 & tstk' & ->).
+    iAssert (⌜ exists tstk_a1 tstk_next', (tstk_next = tstk_a1 :: tstk_next')⌝)%I
+      as %(tstk_a1 & tstk_next' & ->).
     { iEval (rewrite /region_pointsto) in "Htstk".
       iDestruct (big_sepL2_length with "Htstk") as %Hlen_tstk.
       iPureIntro.
@@ -410,10 +421,10 @@ Section Switcher.
       { rewrite finz_seq_between_length.
         assert (a_tstk^+2 < e_tstk)%a by solve_addr.
         rewrite finz_dist_S; last solve_addr.
-        admit.
+        rewrite finz_dist_S; last solve_addr; lia.
       }
-      destruct tstk as [|stk_a1 tstk]; first (cbn in Hlen_tstk; lia).
-      exists stk_a1, tstk; done.
+      destruct tstk_next as [|stk_a1 tstk_next]; first (cbn in Hlen_tstk; lia).
+      exists stk_a1, tstk_next; done.
     }
     iDestruct (region_pointsto_cons with "Htstk") as "[Ha1_tstk Htstk]".
     { transitivity (Some (a_tstk ^+ 2)%a); auto; solve_addr. }
@@ -425,7 +436,7 @@ Section Switcher.
 
     (* Store ct2 csp; *)
     iInstr "Hcode".
-    { solve_addr. }
+    { rewrite /withinBounds andb_true_iff; solve_addr. }
 
     (* WriteSR mtdc ct2; *)
     iInstr "Hcode".
@@ -440,7 +451,7 @@ Section Switcher.
 
     (* Subseg csp cs1 cs0 *)
     iInstr "Hcode".
-    { solve_addr. }
+    { rewrite /isWithin andb_true_iff; solve_addr. }
 
     unfocus_block "Hcode" "Hcont" as "Hcode"; subst hcont.
 
@@ -451,6 +462,11 @@ Section Switcher.
     { admit. }
     iNext ; iIntros "(HPC & Hcsp & Hcs0 & Hcs1 & Hcode & Hstk)".
     unfocus_block "Hcode" "Hcont" as "Hcode"; subst hcont.
+
+
+    (* TODO from here *)
+    (* revoke the world: all temporary invariant will be "frozen" *)
+
 
 
     focus_block 2 "Hcode" as a_unseal Ha_unseal "Hcode" "Hcont"; iHide "Hcont" as hcont.
