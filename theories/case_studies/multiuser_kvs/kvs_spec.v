80dc46eab2ead36746d7cfa00c5168725357dafb
June Rousseau <june.rousseau@cs.au.dk>
2026-05-11 17:28:20 +0200

    kvs spec getFullKey

---
+++ theories/case_studies/multiuser_kvs/kvs_spec.v
@@ -0,0 +1,103 @@
+From iris.proofmode Require Import proofmode.
+From cap_machine Require Import logrel rules.
+From cap_machine Require Import switcher kvs.
+From cap_machine Require Import proofmode.
+
+Section KVS_spec.
+  Context
+    {Σ:gFunctors}
+    {ceriseg:ceriseG Σ}
+    {nainv: logrel_na_invs Σ}
+    {cstackg : CSTACKG Σ}
+    `{MP: MachineParameters}
+    {swlayout : switcherLayout}
+  .
+
+  Definition kvs_map := list (Z * Word).
+
+  Fixpoint isKVS
+    (b e : Addr) (m : kvs_map) : iProp Σ :=
+    match m with
+    | [] => ⌜ b = e ⌝
+    | (k,w)::m' => b ↦ₐ WInt k ∗ (b^+1)%a ↦ₐ w ∗ isKVS (b^+2)%a e m'
+    end
+  .
+
+  Definition kvs_frag (k : Z * Z) (w : Word) : iProp Σ. Admitted.
+  Notation "k ⤇(KVS) w" := (kvs_frag k w) (at level 20) : bi_scope.
+  Global Instance persistent_kvs_frag k w : Persistent (k ⤇(KVS) w)%I.
+  Admitted.
+
+  Lemma KVS_getFullKey_spec `{KVS : kvsLayout}
+    (pc_b pc_e pc_a : Addr)
+    (cgp_b cgp_e : Addr)
+    (rsealkey rkey rscratch : RegName)
+    (user_key nkey : Z)
+    (wscratch : Word)
+    :
+    let instrs := (kvs_getFullKey_instrs rsealkey rsealkey rkey rscratch) in
+    SubBounds pc_b pc_e pc_a (pc_a ^+ length instrs)%a ->
+    withinBounds cgp_b cgp_e cgp_b = true ->
+    (0 <= user_key < top)%Z ->
+
+    rscratch ≠ cnull ->
+    rsealkey ≠ cnull ->
+    rkey ≠ cnull ->
+
+    (
+      PC ↦ᵣ WCap RX Global pc_b pc_e pc_a ∗
+      cgp ↦ᵣ WCap RW Global cgp_b cgp_e cgp_b ∗
+      rsealkey ↦ᵣ kvs_user_seal_key user_key ∗
+      rkey ↦ᵣ WInt nkey ∗
+      rscratch ↦ᵣ wscratch ∗
+
+      cgp_b ↦ₐ kvs_service_unsealing_key ∗
+      codefrag pc_a instrs ∗
+      ▷ (
+          PC ↦ᵣ WCap RX Global pc_b pc_e (pc_a ^+ length instrs)%a ∗
+          cgp ↦ᵣ WCap RW Global cgp_b cgp_e cgp_b ∗
+          rsealkey ↦ᵣ WInt (kvs_full_key user_key nkey) ∗
+          rkey ↦ᵣ WInt nkey ∗
+          rscratch ↦ᵣ kvs_service_unsealing_key ∗
+
+          cgp_b ↦ₐ kvs_service_unsealing_key ∗
+          codefrag pc_a instrs -∗
+
+          WP Seq (Instr Executable) {{ v, ⌜v = HaltedV⌝ → na_own logrel_nais ⊤ }}
+        )
+      ⊢ WP Seq (Instr Executable) {{ v, ⌜v = HaltedV⌝ → na_own logrel_nais ⊤ }})%I.
+  Proof.
+    intros instrs ; subst instrs.
+    iIntros (HsubBounds Hbounds_cgp Hbounds_user_key Hrscratch Hrsealkey Hkey)
+      "(HPC & Hcgp & Hrsealkey & Hrkey & Hrscratch & Hcgp_b & Hcode & Hpost)".
+    codefrag_facts "Hcode"; rename H into Hpc_contiguous ; clear H0.
+
+    (* --------------------------------------------------- *)
+    (* ----------------- Start the proof ----------------- *)
+    (* --------------------------------------------------- *)
+
+    (* load rscratch cgp; *)
+    iInstr "Hcode".
+    { split; done. }
+    iEval (cbn) in "Hrscratch".
+
+    (* unseal rdst rsealkey rscratch; *)
+    iInstr "Hcode"; first done.
+    { rewrite /withinBounds; pose proof KVS_OTYPE_size; solve_addr. }
+    (* geta rdst rdst; *)
+    iInstr "Hcode".
+
+    (* lshiftl rdst rdst 16; *)
+    iInstr "Hcode".
+    (* lor rdst rdst rkey *)
+    iInstr "Hcode".
+
+    replace (Z.lor ((0 ^+ user_key)%a ≪ 16) nkey) with (kvs_full_key user_key nkey).
+    2: {
+      replace (@finz.to_z MemNum (0 ^+ user_key)%a) with user_key; first done.
+      solve_addr.
+    }
+    iApply "Hpost"; iFrame.
+  Qed.
+
+Section KVS_spec.
